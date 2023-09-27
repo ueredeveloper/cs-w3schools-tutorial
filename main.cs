be60fcd8e92fd8f9d2dfd15be35639963833a575
ueredeveloper <ueredeveloper@gmail.com>
2023-09-27 17:01:11 +0000

    inicilização no main

--- main.cs
+++ main.cs
@@ -3,15 +3,8 @@ using System;
 
 class HelloWorld {
   static void Main (string [] args){
-    // quantos comentários WriteLine você quiser
-    Console.WriteLine("Hello");
-    Console.WriteLine("I am learning C#");
-    Console.WriteLine("It is awesome!");
-    // cálculos matemáticos
-    Console.WriteLine(3+3);
-
-    // Write: não insere nova linha
-    Console.Write(" Hello World");
-    Console.Write("I will print on the same line");
+    {
+      Console.WriteLine("Hello World!");    
+    }
   }
 }
