5476092cac45e0788618623cd651996c5ff1712e
ueredeveloper <ueredeveloper@gmail.com>
2023-09-27 16:54:03 +0000

    feat: adicionar novas funcionalidades ao HelloWorld

--- main.cs
+++ main.cs
@@ -1,18 +1,17 @@
-/* Original Replit
 
-using System;
-
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
-}*/
-
-// Teste aula w3Schools
 using System;
 
 class HelloWorld {
   static void Main (string [] args){
-    Console.WriteLine("Hello World, My id is ueredeveloper");
+    // quantos comentários WriteLine você quiser
+    Console.WriteLine("Hello");
+    Console.WriteLine("I am learning C#");
+    Console.WriteLine("It is awesome!");
+    // cálculos matemáticos
+    Console.WriteLine(3+3);
+
+    // Write: não insere nova linha
+    Console.Write(" Hello World");
+    Console.Write("I will print on the same line");
   }
 }
