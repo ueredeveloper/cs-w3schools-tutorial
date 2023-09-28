378a6d22d22fe2dd4ded6f1fd2a80e2de3071ad8
ueredeveloper <ueredeveloper@gmail.com>
2023-09-28 18:42:26 +0000

    chore: Update code comments for improved clarity and readability
    Editando o código com comentários no padrão doxygen

--- main.cs
+++ main.cs
@@ -11,8 +11,9 @@ class HelloWorld
      */
     static void Main(string[] args)
     {
-        // user name
+        // User name
         string name = "José";
+
         // User coordinates
         double lat = -15.123456789;
         double lng = -47.123456789;
@@ -20,15 +21,22 @@ class HelloWorld
         // Determine if the user is granted
         bool isGranted = true;
 
-     string ConvertIsGranted(bool isGranted) {
-      return isGranted ? "Sim" : "Não";
-    }
+        /**
+         * @brief Converts the boolean value to "Sim" (Yes) or "Não" (No).
+         * @param isGranted The boolean value to convert.
+         * @return "Sim" if the input is true, "Não" otherwise.
+         */
+        string ConvertIsGranted(bool isGranted)
+        {
+            return isGranted ? "Sim" : "Não";
+        }
 
-    Console.WriteLine(
-      $" Usuário: {name}" +
-      $"\n Latitude: {lat}" +
-      $"\n Longitude: {lng}" +
-      $"\n Outorgado: {ConvertIsGranted(isGranted)}"
-    );
+        // Display user information
+        Console.WriteLine(
+            $" Usuário: {name}" +
+            $"\n Latitude: {lat}" +
+            $"\n Longitude: {lng}" +
+            $"\n Outorgado: {ConvertIsGranted(isGranted)}"
+        );
     }
 }
